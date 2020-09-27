e99af2764320c27c958bf601a0a80fa56621306b
tocc <tocc.create@gmail.com>
2020-09-27 22:33:10 +0900

    [add / fix] README変更、ABC168 / C問題(WA)

---
+++ ABC/ABC168/c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+constexpr ll INF = 1e18;
+constexpr ll MOD = 1e9 + 7;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define ALL(v) v.begin(), V.end()
+
+int main() {
+  int A, B, H, M;
+  cin >> A >> B >> H >> M;
+  int point[2][2];
+  int degH = H * 30, degM = M * 6;
+
+  cout << degM << endl;
+  // double dist = sqrt(A*A + B*B);
+  // cout << dist << endl;
+}
