160c8d869014293b81e29a10942a28fc193321ab
tocc <tocc.create@gmail.com>
2020-10-01 20:15:14 +0900

    [fix] : template修正

--- template.cpp
+++ template.cpp
@@ -1,8 +1,8 @@
 #pragma region includes
 #include <bits/stdc++.h>
+using namespace std;
 #pragma endregion
 #pragma region defines
-using namespace std;
 using ll = long long;
 using pll = pair<ll, ll>;
 constexpr ll INF = 1e18;
@@ -11,10 +11,8 @@ constexpr ll MOD = 1e9 + 7;
 #define REPR(i, n) for (int i = n; i >= 0; i--)
 #define FOR(i, m, n) for (int i = m; i < n; i++)
 #define ALL(v) v.begin(), V.end()
-template <class T> void CO(T value) { cout << value << endl; }
+template <class T>
+void CO(T value) { cout << value << endl; }
 #pragma endregion
 
-int main() {
-  
-  
-}
+int main() {}
