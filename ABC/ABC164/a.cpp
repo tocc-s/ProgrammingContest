f4e3ce7ab35d7ae35966b3cb3bc608f7b982e105
tocc <tocc.create@gmail.com>
2020-10-04 23:44:01 +0900

    [add] : ABC164 / A問題追加(AC)

---
+++ ABC/ABC164/a.cpp
@@ -0,0 +1,24 @@
+#pragma region includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion
+#pragma region defines
+using ll = long long;
+using pll = pair<ll, ll>;
+constexpr ll INF = 1e18;
+constexpr ll MOD = 1e9 + 7;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define ALL(v) v.begin(), V.end()
+template <class T>
+void CO(T value) { cout << value << endl; }
+#pragma endregion
+
+int main() {
+  int S, W;
+  cin >> S >> W;
+
+  if(W >= S) CO("unsafe");
+  else CO("safe");
+}
