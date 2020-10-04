5963bb7f334a31bbba8c35be203054fceb58f811
tocc <tocc.create@gmail.com>
2020-10-05 04:39:55 +0900

    [add] : ABC177 / B問題追加(AC/WA1回)

--- ABC/ABC177/B.cpp
+++ ABC/ABC177/B.cpp
@@ -1,27 +1,35 @@
+#pragma region includes
 #include <bits/stdc++.h>
 using namespace std;
-// #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#pragma endregion
+#pragma region defines
+using ll = long long;
+using pll = pair<ll, ll>;
+constexpr ll MOD = 1e9 + 7;
+constexpr ll LINF = 1e18;
+constexpr int IINF = 1e9;
+constexpr double PI = 3.14159265358979323846;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define ALL(v) v.begin(), V.end()
+template <class T>
+void CO(T value) {
+  cout << value << endl;
+}
+#pragma endregion
 
 int main() {
   string S, T;
-  cin >> S;
-  cin >> T;
-  int S_len = S.length(), T_len = T.length(), min_cnt = 1001, cnt = 0;
+  cin >> S >> T;
 
-  if (S.find(T) != std::string::npos) {
-    cout << cnt << endl;
-    return 0;
-  }
-
-  for (int i = 0; i < S_len; i++) {
-    for (int j = 0; j < T_len; j++) {
-      if (T[j] == S[i+j]) {
-        cnt++;
-        cout << T[j] << " " << S[i+j] << endl;
-      }
-    }
-    if (min_cnt > cnt) min_cnt = cnt;
+  int cnt = 0, m = 1001;
+  for (int i = 0; i <= S.length() - T.length(); i++) {
     cnt = 0;
+    for (int j = 0; j < T.length(); j++) {
+      if (S[i + j] != T[j]) cnt++;
+    }
+    m = min(m, cnt);
   }
-  cout << min_cnt << endl;
+  CO(m);
 }
