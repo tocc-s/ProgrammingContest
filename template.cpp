9f7a99df691c606a12628818a65ed5b3df4043b0
tocc <tocc.create@gmail.com>
2020-10-07 18:07:29 +0900

    [fix] : template修正

--- template.cpp
+++ template.cpp
@@ -10,11 +10,17 @@ constexpr ll LINF = 1e18;
 constexpr int IINF = 1e9;
 constexpr double PI = 3.14159265358979323846;
 #define REP(i, n) for (int i = 0; i < n; i++)
+#define REPE(i, n) for (int i = 0; i <= n; i++)
 #define REPR(i, n) for (int i = n; i >= 0; i--)
 #define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORR(v, vec) for (auto const& v : vec)
 #define ALL(v) v.begin(), V.end()
 template <class T>
 void CO(T value) { cout << value << endl; }
 #pragma endregion
 
-int main() {}
+int main() {
+  // cout << fixed << setprecision(15);
+  
+  
+}
