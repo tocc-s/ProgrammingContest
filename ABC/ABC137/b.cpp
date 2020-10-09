074f8faf620e683ba76192e7fba475144c433d92
tocc <tocc.create@gmail.com>
2020-10-10 00:03:08 +0900

    [add] : ABC137 / B問題追加(AC)

---
+++ ABC/ABC137/b.cpp
@@ -0,0 +1,36 @@
+#pragma region includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion
+#pragma region defines
+using LL = long long;
+using PLL = pair<LL, LL>;
+using OVEC = vector<int>;
+using TVEC = vector<vector<int>>;
+constexpr LL MOD = 1e9 + 7;
+constexpr LL LINF = 1e18;
+constexpr int IINF = 1e9;
+constexpr double PI = 3.14159265358979323846;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPE(i, n) for (int i = 0; i <= n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORE(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(v, vec) for (auto const& v : vec)
+#define ALL(v) v.begin(), v.end()
+template <class T>
+void CO(T value) {
+  cout << value << endl;
+}
+#pragma endregion
+
+int main() {
+  int k, x;
+  cin >> k >> x;
+
+  FORE(i, x-k+1, x+k-1) {
+    cout << i;
+    if(!(i == x+k-1)) cout << " ";
+    else cout << endl;
+  }
+}
