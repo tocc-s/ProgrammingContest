8c46099686d67d23ee2d4883cc5a11480ba3eb07
tocc <tocc.create@gmail.com>
2020-10-16 11:48:23 +0900

    [add] : template更新

--- template.cpp
+++ template.cpp
@@ -4,15 +4,14 @@ using namespace std;
 #pragma endregion
 #pragma region defines
 using LL = long long;
-using PLL = pair<LL, LL>;
-using VEC = vector<int>;
-using GRAPH = vector<vector<int>>;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using VEC = vector<T>;
+template <class T>
+using GRAPH = vector<vector<T>>;
 using PQ = priority_queue<int>;
 using RPQ = priority_queue<int, vector<int>, greater<int>>;
-constexpr LL MOD = 1e9 + 7;
-constexpr LL LINF = 1e18;
-constexpr int IINF = 1e9;
-constexpr double PI = 3.14159265358979323846;
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REPE(i, n) for (int i = 0; i <= n; i++)
 #define REPR(i, n) for (int i = n; i >= 0; i--)
@@ -21,12 +20,16 @@ constexpr double PI = 3.14159265358979323846;
 #define FORR(v, vec) for (auto const& v : vec)
 #define ALL(v) v.begin(), v.end()
 #define SETPRE(n) cout << fixed << setprecision(n)
+constexpr LL MOD = 1e9 + 7;
+constexpr LL LINF = 1e18;
+constexpr int IINF = 1e9;
+constexpr double PI = 3.14159265358979323846;
+constexpr void string_lower(string& s, int n) { s[n] = s[n] + ('a' - 'A'); }
+constexpr void string_upper(string& s, int n) { s[n] = s[n] - ('a' - 'A'); }
 template <class T>
-void CO(T value) {
+constexpr void CO(T value) {
   cout << value << endl;
 }
-void string_lower(string& s, int n) { s[n] = s[n] + ('a' - 'A'); }
-void string_upper(string& s, int n) { s[n] = s[n] - ('a' - 'A'); }
 #pragma endregion
 
 int main() {
