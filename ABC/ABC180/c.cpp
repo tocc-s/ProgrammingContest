e18561bbc0e338c904483998a8300788caf73e23
tocc <tocc.create@gmail.com>
2020-10-18 18:05:48 +0900

    [add] : ABC180 / C問題(set使用)(解説参考)

---
+++ ABC/ABC180/c.cpp
@@ -0,0 +1,50 @@
+#pragma region includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion
+#pragma region defines
+using LL = long long;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using VEC = vector<T>;
+template <class T>
+using GRAPH = vector<vector<T>>;
+using PQ = priority_queue<int>;
+using RPQ = priority_queue<int, vector<int>, greater<int>>;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPE(i, n) for (int i = 0; i <= n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORE(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(v, vec) for (auto const& v : vec)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+constexpr LL MOD = 1e9 + 7;
+constexpr LL LINF = 1e18;
+constexpr int IINF = 1e9;
+constexpr double PI = 3.14159265358979323846;
+void string_lower(string& s, int n) { s[n] = s[n] + ('a' - 'A'); }
+void string_upper(string& s, int n) { s[n] = s[n] - ('a' - 'A'); }
+template <class T>
+void CO(T value) {
+  cout << value << endl;
+}
+#pragma endregion
+
+int main() {
+  LL n;
+  cin >> n;
+  set<LL> ans; // もし入力が 4 などの場合、約数 2 が出力の際に重複するため、setで解消
+
+  FORE(i, 1, n) {
+    if (i > n / i) break;
+    if (n % i != 0) continue;
+    if (n % i == 0) {
+      ans.insert(i);
+      ans.insert(n / i);
+    }
+  }
+
+  for (auto& itr : ans) CO(itr);
+}
