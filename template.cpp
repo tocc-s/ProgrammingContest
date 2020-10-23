ba2aeefec26e4377f3a38c5da40482bbe6e598c5
tocc <tocc.create@gmail.com>
2020-10-23 17:36:22 +0900

    [fix] : foreach定義 FORR の条件文を変更

--- template.cpp
+++ template.cpp
@@ -18,7 +18,7 @@ using RPQ = priority_queue<int, vector<int>, greater<int>>;
 #define REPR(i, n) for (int i = n; i >= 0; i--)
 #define FOR(i, m, n) for (int i = m; i < n; i++)
 #define FORE(i, m, n) for (int i = m; i <= n; i++)
-#define FORR(itr, dict) for (auto itr = dict.begin(); itr != dict.end(); itr++)
+#define FORR(itr, dict) for (auto& itr : dict)
 #define ALL(v) v.begin(), v.end()
 #define SETPRE(n) cout << fixed << setprecision(n)
 constexpr int MOD = 1e9 + 7;
@@ -37,5 +37,5 @@ void CO(T value) {
 
 
 int main() {
-
+  
 }
