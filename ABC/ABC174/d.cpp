4c5ea580b30ee7d0d65937cad8d987ed2f4d1654
tocc <tocc.create@gmail.com>
2020-10-28 14:08:38 +0900

    [add] : ABC174 / D問題(AC)

---
+++ ABC/ABC174/d.cpp
@@ -0,0 +1,63 @@
+#pragma region Region
+#pragma region Fasts
+#pragma GCC optimize("Ofast")
+#define FAST                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#pragma endregion
+#pragma region Includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion
+#pragma region Defines
+using LL = long long;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using VEC = vector<T>;
+template <class T>
+using GRAPH = vector<vector<T>>;
+using PQ = priority_queue<int>;
+using RPQ = priority_queue<int, vector<int>, greater<int>>;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPE(i, n) for (int i = 0; i <= n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORE(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(itr, dict) for (auto& itr : dict)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+constexpr int MOD = 1e9 + 7;
+constexpr LL LINF = 1e18;
+constexpr int IINF = 1e9;
+constexpr double PI = 3.14159265358979323846;
+void string_lower(string& s, int n) { s[n] = s[n] + ('a' - 'A'); }
+void string_upper(string& s, int n) { s[n] = s[n] - ('a' - 'A'); }
+template <class T>
+void CO(T value) {
+  cout << value << "\n";
+}
+#pragma endregion
+#pragma endregion Region
+///////////////////////////////////////////////////////////////////////////////
+
+int main() {
+  FAST;
+  int n;
+  cin >> n;
+  VEC<char> c(n), tmp(n);
+  REP(i, n) {
+    cin >> c[i];
+    tmp[i] = c[i];
+  }
+  sort(ALL(tmp));
+
+  int cnt = 0;
+  REP(i, n) {
+    if(tmp[i] == 'R') {
+      cnt++;
+      if(c[i] == 'R') cnt--;
+    }
+  }
+  CO(cnt);
+}
