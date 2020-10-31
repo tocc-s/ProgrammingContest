7cd2092e0167f5ee0dfdc358285b3d9eb35106a4
tocc <tocc.create@gmail.com>
2020-11-01 08:12:15 +0900

    [add] : ABC177 / C問題(他者解答参考)

--- ABC/ABC177/C.cpp
+++ ABC/ABC177/C.cpp
@@ -1,18 +1,52 @@
+#pragma region Region
+#pragma region includes
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#pragma endregion
+#pragma region defines
+using LL = long long;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using VEC = vector<T>;
+template <class T>
+using GRAPH = vector<vector<T>>;
+using PQ = priority_queue<int>;
+using RPQ = priority_queue<int, vector<int>, greater<int>>;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPE(i, n) for (int i = 0; i <= n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORE(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(itr, dict) for (auto itr = dict.begin(); itr != dict.end(); itr++)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+constexpr int MOD = 1e9 + 7;
+constexpr LL LINF = 1e18;
+constexpr int IINF = 1e9;
+constexpr double PI = 3.14159265358979323846;
+void string_lower(string& s, int n) { s[n] = s[n] + ('a' - 'A'); }
+void string_upper(string& s, int n) { s[n] = s[n] - ('a' - 'A'); }
+template <class T>
+void CO(T value) {
+  cout << value << endl;
+}
+#pragma endregion
+#pragma endregion Region
+///////////////////////////////////////////////////////////////////////////////
 
 int main() {
-  int N, pow_num = 1000000007;
-  cin >> N;
-  vector<long> A(N);
-  rep(i, N) cin >> A[i];
-  long long ans = 0;
+  int n;
+  cin >> n;
+  VEC<int> a(n);
+  REP(i, n) cin >> a[i];
 
-  for (int i = 0; i < N - 1; i++) {
-    for (int j = i + 1; j < N; j++) {
-      ans += A[i] * A[j];
-    }
+  LL ans = 0, sum = 0;
+  /* 配列iとi-1したものとの掛け算の総和 */
+  REP(i, n) {
+    (ans += a[i] * sum) %= MOD;
+    (sum += a[i]) %= MOD;
   }
-  cout << ans % pow_num << endl;
+
+  CO(ans % MOD);
 }
