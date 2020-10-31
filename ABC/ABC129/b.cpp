e6dbe6ece180d7b89a299fc2bd88bfb1c83d3121
tocc <tocc.create@gmail.com>
2020-11-01 05:04:58 +0900

    [add] : ABC129 / B問題(AC)

---
+++ ABC/ABC129/b.cpp
@@ -0,0 +1,66 @@
+#pragma region Region
+#pragma region Includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion
+#pragma region Setting
+#pragma GCC optimize("Ofast")
+#define FAST                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#define CLS clock_t start = clock();
+#define CLE              \
+  clock_t end = clock(); \
+  cout << end - start << " ms" << '\n';
+#pragma endregion
+#pragma region Defines
+using LL = long long;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using VEC = vector<T>;
+template <class T>
+using GRAPH = vector<vector<T>>;
+using PQ = priority_queue<int>;
+using RPQ = priority_queue<int, vector<int>, greater<int>>;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPE(i, n) for (int i = 0; i <= n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORE(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(itr, dict) for (auto& itr : dict)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+constexpr int MOD = 1e9 + 7;
+constexpr LL LINF = 1e18;
+constexpr int IINF = 1e9;
+constexpr double PI = 3.14159265358979323846;
+void string_lower(string& s, int n) { s[n] = s[n] + ('a' - 'A'); }
+void string_upper(string& s, int n) { s[n] = s[n] - ('a' - 'A'); }
+template <class T>
+void CO(T value) {
+  cout << value << "\n";
+}
+#pragma endregion
+#pragma endregion Region
+///////////////////////////////////////////////////////////////////////////////
+
+int main() {
+  FAST;
+  int n;
+  cin >> n;
+  VEC<int> w(n);
+  REP(i, n) cin >> w[i];
+
+  int mi = IINF;
+  REP(i, n) {
+    int sl = 0, sr = 0;
+
+    REPE(l, i) sl += w[l];
+    FOR(r, i + 1, n) sr += w[r];
+
+    mi = min(mi, abs(sr - sl));
+  }
+
+  CO(mi);
+}
