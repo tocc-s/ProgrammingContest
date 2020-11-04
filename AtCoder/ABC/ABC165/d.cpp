fd54c1489309a6c6aa64b061646766bc14f4b14e
tocc <tocc.create@gmail.com>
2020-11-04 12:41:56 +0900

    [add] : ABC165 / D問題 (Floor Function) / 分からん

---
+++ AtCoder/ABC/ABC165/d.cpp
@@ -0,0 +1,88 @@
+#pragma region Template
+
+#pragma region Includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion Includes
+
+#pragma region Setting
+#pragma GCC optimize("Ofast")
+#define FAST                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#define CLS clock_t start = clock();
+#define CLE              \
+  clock_t end = clock(); \
+  cout << end - start << " ms" << '\n';
+#pragma endregion Setting
+
+#pragma region Defines
+using LL = long long;
+template <class T>
+using P = pair<T, T>;
+template <class T>
+using VEC = vector<T>;
+template <class T>
+using GRAPH = vector<vector<T>>;
+using PQ = priority_queue<int>;
+using RPQ = priority_queue<int, vector<int>, greater<int>>;
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPE(i, n) for (int i = 0; i <= n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORE(i, m, n) for (LL i = m; i <= n; i++)
+#define FREV(i, m, n) for (int i = m; i >= n; i--)
+#define FORR(itr, dict) for (auto& itr : dict)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+#define en '\n'
+constexpr LL LLINF = LLONG_MAX;
+constexpr int IINF = INT_MAX;
+constexpr int MOD = 1e9 + 7;
+constexpr double PI = 3.14159265358979323846;
+#pragma endregion Defines
+
+#pragma region Functions
+void string_lower(string& s, int n) { s[n] = s[n] + ('a' - 'A'); }
+void string_upper(string& s, int n) { s[n] = s[n] - ('a' - 'A'); }
+template <class T>
+void CO(T value) {
+  cout << value << en;
+}
+void CO(void) { cout << en; }
+int ctoi(char c) {
+  if ('0' <= c && c <= '9')
+    return (c - '0');
+  else {
+    CO("ctoi error!");
+    return -1;
+  }
+}
+#pragma endregion Functions
+
+#pragma endregion Template
+///////////////////////////////////////////////////////////////////////////////
+
+LL floor_diff(LL a, LL b, LL n) {
+  LL res = -LLINF;
+  FORE(x, 1, n) {
+    LL tmp = floor(a * x / b) - a * floor(x / b);
+    res = max(res, tmp);
+    tmp = floor(a * (n - x) / b) - a * floor((n - x) / b);
+    res = max(res, tmp);
+    // if (x >= n - x) break;
+  }
+  // LL res = floor(a * n / b) - a * floor(n / b);
+  // CO((LL)floor(a * n / b));
+  // CO((LL)(a * floor(n / b)));
+
+  return res;
+}
+
+int main() {
+  FAST;
+  LL a, b, n;
+  cin >> a >> b >> n;
+
+  CO(floor_diff(a, b, n));
+}
