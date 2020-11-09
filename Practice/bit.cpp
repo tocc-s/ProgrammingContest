f2018896723b93161658d08caebc1ea7b16576cd
tocc <tocc.create@gmail.com>
2020-11-09 17:12:47 +0900

    [add] : 部分和問題 (bit全探索) 問題文追加

--- Practice/bit.cpp
+++ Practice/bit.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 /* 部分和問題 */
+/* n 個の正の整数 a[0],a[1],…,a[n−1]a[0],a[1],…,a[n−1] と正の整数 W
+ * が与えられる。これらの整数から何個かの整数を選んで総和が W
+ * になるようにすることが可能か判定せよ。可能ならば "YES" と出力し、不可能ならば
+ * "NO" と出力せよ。*/
 
 void YorN(bool flag) {
-  if(flag) cout << "Yes" << endl;
-  else cout << "No" << endl;
+  if (flag)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
 }
 
 int main() {
@@ -19,12 +25,12 @@ int main() {
   for (int i = 0; i < (1 << N); i++) {
     int sum = 0;
     for (int j = 0; j < N; j++) {
-      //iを2進数表記で表した時、j番目にbitが立っていたら
-      if(i & (1 << j)) sum += a[j];
+      // iを2進数表記で表した時、j番目にbitが立っていたら
+      if (i & (1 << j)) sum += a[j];
     }
 
-    if(sum == W) flag = true;
+    if (sum == W) flag = true;
   }
-  
+
   YorN(flag);
 }
