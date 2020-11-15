9699d647ff98ad6dce92860e68db155a063b0d87
tocc <tocc.create@gmail.com>
2020-11-16 05:40:06 +0900

    [fix] : 自己解釈修正

--- Practice/knapsack_dp.cpp
+++ Practice/knapsack_dp.cpp
@@ -95,6 +95,8 @@ void solve() {
   REP(i, n) {
     REPE(j, W) {
       if(j < w[i]) dp[i + 1][j] = dp[i][j];
+      /*  重さの指標がi番目の(入れている)荷物を超えたなら、
+          その重さになる限界の荷物の価値を、ナップザック格納できる ? */
       else dp[i + 1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
     }
   }
