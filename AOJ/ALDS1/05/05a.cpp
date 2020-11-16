9e42d2c9e62502fde01e06b09da52a6408f2f2ce
tocc <tocc.create@gmail.com>
2020-11-16 21:43:00 +0900

    [add] : ALDS1 / 05a (部分和, 配列要素と一致)

---
+++ AOJ/ALDS1/05/05a.cpp
@@ -0,0 +1,104 @@
+#pragma region Template
+#pragma region Includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion Includes
+#pragma region Setting
+#pragma GCC optimize("Ofast")
+#define FAST                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#pragma endregion Setting
+#pragma region Defines
+using LL = long long;
+template<class T> using P = pair<T, T>;
+template<class T> using VEC = vector<T>;
+template<class T> using VEC2 = vector<vector<T>>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using RPQ = priority_queue<T, vector<T>, greater<T>>;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPE(i, n) for(int i = 0; i <= n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define FORE(i, m, n) for(int i = m; i <= n; i++)
+#define FREV(i, m, n) for(int i = m; i >= n; i--)
+#define FORR(itr, dict) for(auto &itr : dict)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+#define en '\n'
+constexpr LL LLINF = LLONG_MAX;
+constexpr int IINF = INT_MAX;
+constexpr int MOD = 1e9 + 7;
+constexpr double PI = 3.14159265358979323846;
+#pragma endregion Defines
+#pragma region Functions
+void string_lower(string &s, int n) {
+  if(s[n] >= 65 && s[n] <= 90) s[n] = s[n] + ('a' - 'A');
+}
+void string_upper(string &s, int n) {
+  if(s[n] >= 97 && s[n] <= 122) s[n] = s[n] - ('a' - 'A');
+}
+template<class T> inline void CO(T value) { cout << value << en; }
+inline void CO(void) { cout << en; }
+template<class T> inline void CON(T value) { cout << value; }
+int ctoi(char c) {
+  if('0' <= c && c <= '9') return (c - '0');
+  else {
+    CO("ctoi error!");
+    return -1;
+  }
+}
+char itoc(int n) { return n + '0'; }
+template<class T> inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool int_chk(T n) {
+  if(ceil(n) == floor(n)) return true;
+  return false;
+}
+#pragma endregion Functions
+#pragma endregion Template
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void solve() {
+  int n, q;
+  cin >> n;
+  VEC<int> a(n);
+  REP(i, n) cin >> a[i];
+  cin >> q;
+  VEC<int> m(q);
+  REP(i, q) cin >> m[i];
+  VEC<bool> flag(q, false);
+  
+  for(int bit = 0; bit < (1 << n); bit++) {
+    int sum = 0;
+    REP(i, n) {
+      if(bit & (1 << i)) sum += a[i];
+    }
+
+    REP(i, q) {
+      if(m[i] == sum) flag[i] = true;
+    }
+  }
+
+  REP(i, q) {
+    if(flag[i]) CO("yes");
+    else CO("no");
+  }
+}
+
+int main() {
+  FAST;
+  solve();
+}
