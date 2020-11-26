577dad8915e489a99c397e4fee7471c46e9e149c
tocc <tocc.create@gmail.com>
2020-11-26 23:56:36 +0900

    [add] : ALDS1 / 11 / D [DFS] [add] : ALDS1 / 11 / D [UnionFind]

---
+++ AOJ/ALDS1/11/d.cpp
@@ -0,0 +1,115 @@
+#pragma region Template
+#pragma region Includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion Includes
+#pragma region Setting
+#pragma GCC optimize("Ofast")
+#define FAST                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#pragma endregion Setting
+#pragma region Defines
+using LL = long long;
+template<class T> using P = pair<T, T>;
+template<class T> using VEC = vector<T>;
+template<class T> using VEC2 = vector<vector<T>>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQR = priority_queue<T, vector<T>, greater<T>>;
+#define REP(i, m, n) for(int i = m; i < (int)(n); i++)
+#define REPE(i, m, n) for(int i = m; i <= (int)(n); i++)
+#define REPR(i, m, n) for(int i = m; i >= (int)(n); i--)
+#define FORR(itr, dict) for(auto &itr : dict)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+#define en '\n'
+constexpr LL LLINF = LLONG_MAX;
+constexpr int IINF = INT_MAX;
+constexpr int MOD = 1e9 + 7;
+constexpr double PI = 3.14159265358979323846;
+#pragma endregion Defines
+#pragma region Functions
+void string_lower(string &s, int n) {
+  if(s[n] >= 65 && s[n] <= 90) s[n] = s[n] + ('a' - 'A');
+}
+void string_upper(string &s, int n) {
+  if(s[n] >= 97 && s[n] <= 122) s[n] = s[n] - ('a' - 'A');
+}
+template<class T> inline void CO(T value) { cout << value << en; }
+inline void CO(void) { cout << en; }
+template<class T> inline void CON(T value) { cout << value; }
+int ctoi(char c) {
+  if('0' <= c && c <= '9') return (c - '0');
+  else {
+    CO("ctoi error!");
+    return -1;
+  }
+}
+char itoc(int n) { return n + '0'; }
+template<class T> inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool int_chk(T n) {
+  if(ceil(n) == floor(n)) return true;
+  return false;
+}
+#pragma endregion Functions
+#pragma endregion Template
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int c = 0;
+
+void dfs(int idx, VEC2<int> &G, VEC<int> &color, int c) {
+  if(color[idx] != 0) return;  // 探索済みならスルー
+
+  color[idx] = c;  // 到達済み、探索未完了
+
+  REP(i, 0, G[idx].size()) {
+    int next = G[idx][i];
+    if(color[next] != 0) continue;  // 隣接頂点が探索済みならスルー
+
+    if(color[next] == 0) {     // 隣接頂点が未到達なら
+      dfs(next, G, color, c);  // 未到達の隣接頂点を探索
+    }
+  }
+}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  VEC2<int> G(n);
+  VEC<int> color(n, 0);
+  int s, t;
+  REP(i, 0, m) {
+    cin >> s >> t;
+    G[s].emplace_back(t);
+    G[t].emplace_back(s);
+  }
+
+  REP(i, 0, n) { dfs(i, G, color, ++c); }
+
+  int q;
+  cin >> q;
+  REP(i, 0, q) {
+    cin >> s >> t;
+    if(color[s] == color[t]) CO("yes");
+    else
+      CO("no");
+  }
+}
+
+int main() {
+  FAST;
+  solve();
+}
