e1d116f43f094093462ca8ad13bcc55b47a33b97
tocc <tocc.create@gmail.com>
2020-12-25 23:10:12 +0900

    [add] : ABC138 / B (AC)

---
+++ AtCoder/ABC/ABC138/b.cpp
@@ -0,0 +1,104 @@
+#pragma region Template
+#pragma region Includes
+#include <bits/stdc++.h>
+using namespace std;
+#pragma endregion Includes
+#pragma region Setting
+#pragma GCC optimize("Ofast")
+#define FAST                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#pragma endregion Setting
+#pragma region Defines
+using LL = long long;
+template<class T> using P = pair<T, T>;
+template<class T> using VEC = vector<T>;
+template<class T> using VEC2 = vector<vector<T>>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQR = priority_queue<T, vector<T>, greater<T>>;
+#define REP(i, m, n) for(int i = m; i < (int)(n); i++)
+#define REPE(i, m, n) for(int i = m; i <= (int)(n); i++)
+#define REPR(i, m, n) for(int i = m; i >= (int)(n); i--)
+#define FORR(itr, dict) for(auto &itr : dict)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+#define en '\n'
+constexpr LL LLINF = LLONG_MAX;
+constexpr int IINF = INT_MAX;
+constexpr int MOD = 1e9 + 7;
+constexpr double PI = 3.14159265358979323846;
+#pragma endregion Defines
+#pragma region Functions
+void string_lower(string &s, int n) {
+  if(s[n] >= 65 && s[n] <= 90) s[n] = s[n] + ('a' - 'A');
+}
+void string_upper(string &s, int n) {
+  if(s[n] >= 97 && s[n] <= 122) s[n] = s[n] - ('a' - 'A');
+}
+template<class T> inline void CO(T value) { cout << value << en; }
+inline void CO(void) { cout << en; }
+template<class T> inline void CON(T value) { cout << value; }
+int ctoi(char c) {
+  if('0' <= c && c <= '9') return (c - '0');
+  else {
+    CO("ctoi error!");
+    return -1;
+  }
+}
+char itoc(int n) { return n + '0'; }
+template<class T> inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool int_chk(T n) {
+  if(ceil(n) == floor(n)) return true;
+  return false;
+}
+#pragma endregion Functions
+#pragma endregion Template
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* 最大公約数 */
+template<class T> 
+T gcd(T x, T y) {
+  if (x % y == 0) return y;
+  else return gcd(y, x % y);
+}
+
+/* 最小公倍数 */
+template<class T> 
+T lcm(T x, T y) {
+  return x * y / gcd(x, y);
+}
+
+void solve() {
+  SETPRE(14);
+  int n;
+  cin >> n;
+  VEC<int> a(n);
+  REP(i, 0, n) cin >> a[i];
+
+  int tmp = a[0];
+  REP(i, 1, n) {
+    tmp = lcm(tmp, a[i]);
+  }
+  double diff = 0;
+  REP(i, 0, n) diff += tmp / a[i];
+
+  CO(tmp / diff);
+}
+
+int main() {
+  FAST;
+  solve();
+}
