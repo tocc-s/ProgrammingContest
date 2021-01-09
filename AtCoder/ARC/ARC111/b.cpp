4b93158354f9164df418870c756a553473901aec
tocc <tocc.create@gmail.com>
2021-01-09 23:05:36 +0900

    [add] : ARC111 / A 分からん / B 分からん

---
+++ AtCoder/ARC/ARC111/b.cpp
@@ -0,0 +1,104 @@
+#pragma region Template
+#pragma region Includes
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+#pragma endregion Includes
+#pragma region Setting
+#pragma GCC optimize("Ofast")
+#define FAST                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+#pragma endregion Setting
+#pragma region Defines
+using LL = long long;
+template<class T> using P = pair<T, T>;
+template<class T> using VEC = vector<T>;
+template<class T> using VEC2 = vector<vector<T>>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQR = priority_queue<T, vector<T>, greater<T>>;
+#define REP(i, m, n) for(int i = m; i < (int)(n); i++)
+#define REPE(i, m, n) for(int i = m; i <= (int)(n); i++)
+#define REPR(i, m, n) for(int i = m; i >= (int)(n); i--)
+#define FORR(itr, dict) for(auto &itr : dict)
+#define ALL(v) v.begin(), v.end()
+#define SETPRE(n) cout << fixed << setprecision(n)
+#define en '\n'
+constexpr LL LLINF = LLONG_MAX;
+constexpr int IINF = INT_MAX;
+constexpr int MOD = 1e9 + 7;
+constexpr double PI = 3.14159265358979323846;
+#pragma endregion Defines
+#pragma region Functions
+void string_lower(string &s, int n) {
+  if(s[n] >= 65 && s[n] <= 90) s[n] = s[n] + ('a' - 'A');
+}
+void string_upper(string &s, int n) {
+  if(s[n] >= 97 && s[n] <= 122) s[n] = s[n] - ('a' - 'A');
+}
+template<class T> inline void CO(T value) { cout << value << en; }
+inline void CO(void) { cout << en; }
+template<class T> inline void CON(T value) { cout << value; }
+int ctoi(char c) {
+  if('0' <= c && c <= '9') return (c - '0');
+  else {
+    CO("ctoi error!");
+    return -1;
+  }
+}
+char itoc(int n) { return n + '0'; }
+template<class T> inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool int_chk(T n) {
+  if(ceil(n) == floor(n)) return true;
+  return false;
+}
+#pragma endregion Functions
+#pragma endregion Template
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void solve() {
+  int n;
+  cin >> n;
+  // VEC<int> a(n), b(n);
+  unordered_set<int> a_dict, b_dict, main_dict;
+
+  int cnt = 0, a, b, m_cnt, a_cnt, b_cnt;
+  REP(i, 0, n) {
+    bool flag_a = false, flag_b = false, flag_m = false;
+    m_cnt = main_dict.size();
+    a_cnt = a_dict.size();
+    b_cnt = b_dict.size();
+    cin >> a >> b;
+
+    a_dict.insert(a);
+    if(a_cnt != a_dict.size()) flag_a = true;
+    b_dict.insert(b);
+    if(b_cnt != b_dict.size()) flag_b = true;
+    main_dict.insert(a);
+    main_dict.insert(b);
+
+    // if(m_cnt != main_dict.size()) flag_m = true;
+
+    if((flag_a || flag_b)) cnt++;
+  }
+
+  CO(cnt);
+}
+
+int main() {
+  FAST;
+  solve();
+}
