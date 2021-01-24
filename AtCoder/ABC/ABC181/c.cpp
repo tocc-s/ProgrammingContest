03d84c60e49df9341b5ed22ed1ad1a979f9b0785
tocc <tocc.create@gmail.com>
2021-01-25 04:10:03 +0900

    [add] : ABC181 / C 分からん

--- AtCoder/ABC/ABC181/c.cpp
+++ AtCoder/ABC/ABC181/c.cpp
@@ -1,64 +1,96 @@
-#pragma region Region
+#pragma region Template
 #pragma region Includes
 #include <bits/stdc++.h>
+#include <unordered_set>
 using namespace std;
-#pragma endregion
+#pragma endregion Includes
 #pragma region Setting
 #pragma GCC optimize("Ofast")
 #define FAST                   \
   ios::sync_with_stdio(false); \
   cin.tie(nullptr);
-#define CLS clock_t start = clock();
-#define CLE              \
-  clock_t end = clock(); \
-  cout << end - start << " ms" << '\n';
-#pragma endregion
+#pragma endregion Setting
 #pragma region Defines
 using LL = long long;
-template <class T>
-using P = pair<T, T>;
-template <class T>
-using VEC = vector<T>;
-template <class T>
-using GRAPH = vector<vector<T>>;
-using PQ = priority_queue<int>;
-using RPQ = priority_queue<int, vector<int>, greater<int>>;
-#define REP(i, n) for (int i = 0; i < n; i++)
-#define REPE(i, n) for (int i = 0; i <= n; i++)
-#define REPR(i, n) for (int i = n; i >= 0; i--)
-#define FOR(i, m, n) for (int i = m; i < n; i++)
-#define FORE(i, m, n) for (int i = m; i <= n; i++)
-#define FORR(itr, dict) for (auto& itr : dict)
+template<class T> using P = pair<T, T>;
+template<class T> using VEC = vector<T>;
+template<class T> using VEC2 = vector<vector<T>>;
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQR = priority_queue<T, vector<T>, greater<T>>;
+#define REP(i, m, n) for(int i = m; i < (int)(n); i++)
+#define REPE(i, m, n) for(int i = m; i <= (int)(n); i++)
+#define REPR(i, m, n) for(int i = m; i >= (int)(n); i--)
+#define FORR(itr, dict) for(auto &itr : dict)
 #define ALL(v) v.begin(), v.end()
 #define SETPRE(n) cout << fixed << setprecision(n)
+#define en '\n'
 constexpr LL LLINF = LLONG_MAX;
 constexpr int IINF = INT_MAX;
 constexpr int MOD = 1e9 + 7;
 constexpr double PI = 3.14159265358979323846;
-void string_lower(string& s, int n) { s[n] = s[n] + ('a' - 'A'); }
-void string_upper(string& s, int n) { s[n] = s[n] - ('a' - 'A'); }
-template <class T>
-void CO(T value) {
-  cout << value << "\n";
+#pragma endregion Defines
+#pragma region Functions
+void string_lower(string &s, int n) {
+  if(s[n] >= 65 && s[n] <= 90) s[n] = s[n] + ('a' - 'A');
 }
-#pragma endregion
-#pragma endregion Region
-///////////////////////////////////////////////////////////////////////////////
+void string_upper(string &s, int n) {
+  if(s[n] >= 97 && s[n] <= 122) s[n] = s[n] - ('a' - 'A');
+}
+template<class T> inline void CO(T value) { cout << value << en; }
+inline void CO(void) { cout << en; }
+template<class T> inline void CON(T value) { cout << value; }
+int ctoi(char c) {
+  if('0' <= c && c <= '9') return (c - '0');
+  else {
+    CO("ctoi error!");
+    return -1;
+  }
+}
+char itoc(int n) { return n + '0'; }
+template<class T> inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool int_chk(T n) {
+  if(ceil(n) == floor(n)) return true;
+  return false;
+}
+#pragma endregion Functions
+#pragma endregion Template
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int main() { 
-  FAST;
+void solve() {
   int n;
   cin >> n;
-  VEC<int> x(n), y(n);
-  REP(i, n) {
-    cin >> x[i] >> y[i];
+  int x, y;
+  VEC<int> cx(1010, 0), cy(1010, 0);
+  REP(i, 0, n) {
+    cin >> x >> y;
+    cx[x]++;
+    cy[y]++;
   }
 
-  int cnt = 0;
-  // REP(i, n) {
-  //   if
-  //   REP(j, n) {
-  //     if()
-  //   }
-  // }
+  REP(i, 0, 1010) {
+    if(cx[i] >= 3 || cy[i] >= 3) {
+      CO("Yes");
+      return;
+    }
+  }
+
+  CO("No");
+}
+
+int main() {
+  FAST;
+  solve();
 }
